93e4d07c41e959b0074c5643a185bdb256e7a1c9
Czzino <fvwlharu@naver.com>
2024-08-06 18:06:35 +0900

    springbootwork

---
+++ Ex05_jspMybatis/src/main/java/com/study/springboot/controller/BoardController.java
@@ -0,0 +1,19 @@
+package com.study.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BoardController {
+	
+	@RequestMapping("/")
+	public String root() throws Exception {
+		return "redirect:list";
+	}
+	
+	@RequestMapping("/")
+	public String list() {
+		return "list";
+	}
+}
